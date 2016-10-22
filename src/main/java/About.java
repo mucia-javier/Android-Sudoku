d576788a0cc2ad0876a23c4086789ace53a4aa3a
Javier <javcoz@yahoo.com>
2016-10-22 13:59:51 -0400

    Create About.java

---
+++ src/main/java/About.java
@@ -0,0 +1,15 @@
+package com.example.javcoz.sudoku;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
